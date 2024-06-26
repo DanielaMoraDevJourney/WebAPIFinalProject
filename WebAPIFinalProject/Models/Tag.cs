b83902fa4a883ba90cee5e404680ff791eed8fb7
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-25 20:49:04 -0500

    Model Tag

---
+++ WebAPIFinalProject/Models/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIFinalProject.Models
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Nombre { get; set; }
+
+        [Required]
+        public string? DisplayNombre { get; set; }
+
+    }
+}
