65d64d06d26723a2b41b6901205a2f22ce423be6
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-25 21:15:57 -0500

    Paso de mis entidades Modelos a mi Context

--- WebAPIFinalProject/Data/ApplicationDbContext.cs
+++ WebAPIFinalProject/Data/ApplicationDbContext.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebAPIFinalProject.Models;
 
 namespace WebAPIFinalProject.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+        //Modelos
+
+        public DbSet<Tag> Tags { get; set; }
+
     }
 }
